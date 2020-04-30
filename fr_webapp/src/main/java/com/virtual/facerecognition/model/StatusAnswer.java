4457fa6777fbbd034a127edf0a5b5679bc6b64bb
d.dmitriev <dds98@mail.ru>
2020-04-30 21:07:36 +0300

    Added status answer model for storage service and env

---
+++ fr_webapp/src/main/java/com/virtual/facerecognition/model/StatusAnswer.java
@@ -0,0 +1,13 @@
+package com.virtual.facerecognition.model;
+
+public class StatusAnswer {
+    private boolean status;
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
