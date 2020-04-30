4c2c10975a79b4f7e513ca6d0286023aeeb3b59e
d.dmitriev <dds98@mail.ru>
2020-04-30 14:40:59 +0300

    Changed ip to webapp

--- fr_frontend/src/components/Recognition.js
+++ fr_frontend/src/components/Recognition.js
@@ -18,7 +18,7 @@ class Recognition extends Component {
 
       var files = document.getElementById('recognitionUpload').files;
 
-      if(files.length > 0) {
+      if (files.length > 0) {
         this.setState({
           selectedFile: files[0],
           imagePreviewUrl: URL.createObjectURL(files[0]),
@@ -29,36 +29,50 @@ class Recognition extends Component {
   }
 
   recognitionHandler = event => {
-    if(this.state.selectedFile == null) return;
+    if (this.state.selectedFile == null) return;
 
     var formData = new FormData();
     formData.append('file', this.state.selectedFile);
 
-    fetch('http://localhost:8080/recognize', {
+    fetch('http://172.17.184.34:32710/recognize', {
       method: 'POST',
       headers: {
-          // 'Content-Type': 'multipart/form-data'
+        // 'Content-Type': 'multipart/form-data'
       },
       body: formData
     })
       .then(response => response.json())
-      .then(result => {this.setState(result)});
+      .then(result => { this.setState(result) });
   }
 
   render() {
+    var personData = <div className="None"></div>;
+
+    if (this.state.result != null) {
+      personData = <div>Лицо найдено: {this.state.result.faceIsFoundInImage.toString()}</div>;
+
+      if (this.state.result.personData != null) {
+        personData += <div>Кто на фотографии: {this.state.result.personData.lastName} {this.state.result.personData.firstName}</div>;
+
+      //   if (this.state.result.personData.age != null)
+      //     personData += <div>Возраст: {this.state.result.personData.age}</div>;
+
+      //   if (this.state.result.personData.externalId != null)
+      //     personData += <div>Внешний айди: {this.state.result.personData.externalId}</div>;
+      } else{}
+      //   personData += <div>Данного человека в БД нет</div>;
+    }
 
     return (
       <div className="Recognition">
         <div className="content">
           <div className="people"><img src={this.state.imagePreviewUrl} alt="People" /></div>
           <div className="recognitionData">
-            {this.state.result != null ? 
-              <div>Лицо найдено: {this.state.result.faceIsFoundInImage.toString()} <br></br> Кто на фотографии: {this.state.result.personData.firstName}</div> :
-             <div className="None"></div>}
+            {personData}
             <UploadButton id="recognitionUpload" value="Upload photo" onClick={this.fileUploadHandler}>
             </UploadButton>
             <button onClick={this.recognitionHandler}>
-                Recognize
+              Recognize
             </button>
           </div>
         </div>
